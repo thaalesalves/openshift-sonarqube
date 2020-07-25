f0b8f1da188ce1d1fd7a34c20537acd09f52852e
Thales Alves <thales@thalesalv.es>
2020-07-25 02:46:06 +0100

    image optimization: still needs work because bitnami sonarqube doesnt work without root; readme updated; create docker compose for the images5D

--- Docker/sonarqube/prebuildfs/build/bitnami-user.sh
+++ Docker/sonarqube/prebuildfs/build/bitnami-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-useradd -ms /bin/bash bitnami
+useradd -u 1001 -ms /bin/bash bitnami
 mkdir -p /opt/bitnami
 sed -i -e 's/\s*Defaults\s*secure_path\s*=/# Defaults secure_path=/' /etc/sudoers
 echo 'bitnami ALL=NOPASSWD: ALL' >> /etc/sudoers
--- Docker/sonarqube/rootfs/app-entrypoint.sh
+++ Docker/sonarqube/rootfs/app-entrypoint.sh
@@ -5,6 +5,12 @@
 
 print_welcome_page
 
+while ! nc -z postgresql 5432; do
+  info "Waiting for PostgreSQL to be up...";
+  sleep 2;
+done;
+info "PostgreSQL is up. Proceeding.";
+
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     nami_initialize postgresql-client sonarqube
     info "Starting gosu... "
