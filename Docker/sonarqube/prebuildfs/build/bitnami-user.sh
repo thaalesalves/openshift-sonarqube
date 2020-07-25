128e243960f1d5b847c34eba120c68e543b15490
Thales Alves <thales@thalesalv.es>
2020-07-25 22:51:29 +0100

    created new sonarqube image from scratch: no longer using bitnami

--- Docker/sonarqube/prebuildfs/build/bitnami-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-useradd -u 1001 -ms /bin/bash bitnami
-mkdir -p /opt/bitnami
-sed -i -e 's/\s*Defaults\s*secure_path\s*=/# Defaults secure_path=/' /etc/sudoers
-echo 'bitnami ALL=NOPASSWD: ALL' >> /etc/sudoers
