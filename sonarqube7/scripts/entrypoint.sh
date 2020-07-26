f38fcc538abdcdf8794db14c160398919bef3e2a
Thales Alves <thales@thalesalv.es>
2020-07-26 23:38:19 +0100

    image optimized: now working on openshift

--- sonarqube7/scripts/entrypoint.sh
+++ sonarqube7/scripts/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "sonar.jdbc.username=$SONAR_JDBC_USERNAME" >> /opt/sonarqube/conf/sonar.properties
+echo "sonar.jdbc.password=$SONAR_JDBC_PASSWORD" >> /opt/sonarqube/conf/sonar.properties
+echo "sonar.jdbc.url=$SONAR_JDBC_URL" >> /opt/sonarqube/conf/sonar.properties
+echo "sonar.path.data=$SONAR_ELASTISEARCH_DIR" >> /opt/sonarqube/conf/sonar.properties
+echo "sonar.path.temp=$SONAR_TEMP_DIR" >> /opt/sonarqube/conf/sonar.properties
+
 sonar.sh start
 echo -e "==========================================================="
 echo -e "SonarQube $SONARQUBE_VERSION is starting!"
